8a07c4bbe1b6b4d260b9a704e57b1dda15e1c73c
Ethan Sharp <ethansharpdev@gmail.com>
2022-05-31 12:26:09 -0600

    port

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ app.post("/api/preflop", preflopCalc);
 app.post("/api/pair", pairWinCalc);
 
 
-app.listen(3000, () => {
-    console.log('Running on port 3000')
+port = process.env.PORT || 3000
+
+app.listen(port , () => {
+    console.log(`Running on ${port}`)
 });
